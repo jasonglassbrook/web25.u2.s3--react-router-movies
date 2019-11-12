d9be68ecd64f83b020a419e7764c4a8e1bd8aac0
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-12 17:54:49 -0500

    format code + whitespace

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 ***************************************/
 const Movie = (props) => {
   /// states ///
-  const [movie , setMovie] = React.useState ();
+  const [movie , setMovie] = React.useState ({});
 
   /// effects ///
   React.useEffect (() => {
@@ -15,16 +15,15 @@ const Movie = (props) => {
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get (`http://localhost:5000/api/movies/${id}`)
-        .then ((response) => {
-          setMovie (response.data);
-        })
-        .catch ((error) => {
-          console.error (error);
-        });
-
-  },[]);
+    axios
+      .get (`http://localhost:5000/api/movies/${id}`)
+      .then ((response) => {
+        setMovie (response.data);
+      })
+      .catch ((error) => {
+        console.error (error);
+      });
+  } , []);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
