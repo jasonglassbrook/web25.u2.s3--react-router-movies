c9549626fb28553278293b57f16927208eb6b590
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-12 17:39:44 -0500

    convert to block arrow fn

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -4,15 +4,18 @@ import React from 'react';
 /***************************************
   COMPONENTS
 ***************************************/
-const SavedList = (props) => (
-  <div className="saved-list">
-    <h3>Saved Movies:</h3>
-    {props.list.map (movie => (
-      <span className="saved-movie">{movie.title}</span>
-    ))}
-    <div className="home-button">Home</div>
-  </div>
-);
+const SavedList = (props) => {
+  /// this ///
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map (movie => (
+        <span className="saved-movie">{movie.title}</span>
+      ))}
+      <div className="home-button">Home</div>
+    </div>
+  );
+};
 
 /**************************************/
 export default SavedList;
