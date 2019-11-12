6e281c4c6f3bd0391a62d13ef1a0450f3c56b8e3
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-12 17:25:23 -0500

    whitespace

--- client/src/index.js
+++ client/src/index.js
@@ -11,4 +11,4 @@ import './index.css';
 /***************************************
   APP
 ***************************************/
-ReactDOM.render (<App /> , document.getElementById ('root'));
+ReactDOM.render (<App/> , document.getElementById ('root'));
